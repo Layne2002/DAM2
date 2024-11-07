d68c747f4791b2f119a86c82082a822f3b9af9e6
Karim <layne_2002@icloud.com>
2024-11-07 17:14:58 +0100

    Contador

--- DiseñoDeInterfaces/02-Bases/src/app/app.component.ts
+++ DiseñoDeInterfaces/02-Bases/src/app/app.component.ts
@@ -4,10 +4,30 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = '02-Bases';
+  public title: string = 'Mi primera app de Angular';
+  public contador: number = 10;
+  public intervalo: number = 1;
+
+  aumentarIntervalo(): void {
+    this.intervalo += 1;
+  }
+  disminuirIntervalo(): void {
+    this.intervalo -= 1;
+  }
+  reestablecerIntervalo(): void {
+    this.intervalo = 1;
+  }
+  incrementarPor(): void {
+    this.contador += this.intervalo;
+  }
+  disminuirPor(): void {
+    this.contador -= this.intervalo;
+  }
+  reestablecerContador(): void {
+    this.contador = 0;
+  }
 }
