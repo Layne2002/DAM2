e21549414faaf26fbdfeb426856b04c1f456f726
Karim <layne_2002@icloud.com>
2024-11-07 19:01:09 +0100

    Ejercicio Contador Angular

--- DiseñoDeInterfaces/02-Bases/src/app/app.component.ts
+++ DiseñoDeInterfaces/02-Bases/src/app/app.component.ts
@@ -1,33 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContadorComponent } from './contador/contador.components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [ContadorComponent],
 })
 export class AppComponent {
   public title: string = 'Mi primera app de Angular';
-  public contador: number = 10;
-  public intervalo: number = 1;
-
-  aumentarIntervalo(): void {
-    this.intervalo += 1;
-  }
-  disminuirIntervalo(): void {
-    this.intervalo -= 1;
-  }
-  reestablecerIntervalo(): void {
-    this.intervalo = 1;
-  }
-  incrementarPor(): void {
-    this.contador += this.intervalo;
-  }
-  disminuirPor(): void {
-    this.contador -= this.intervalo;
-  }
-  reestablecerContador(): void {
-    this.contador = 0;
-  }
 }
---
+++ DiseñoDeInterfaces/02-Bases/src/app/contador/contador.components.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contador',
+  template: `
+    <h3>Contador: {{ contador }}</h3>
+    <button (click)="incrementarPor()">+ {{ intervalo }}</button>
+    <button (click)="disminuirPor()">- {{ intervalo }}</button>
+    <button (click)="reestablecerContador()">RESET</button>
+    <br />
+    <br />
+    <button (click)="aumentarIntervalo()">Aumentar intervalo</button>
+    <button (click)="disminuirIntervalo()">Disminuir intervalo</button>
+    <button (click)="reestablecerIntervalo()">Reestablecer intervalo</button>
+    <hr />
+  `,
+  standalone: true,
+})
+export class ContadorComponent {
+  public contador: number = 10;
+  public intervalo: number = 1;
+
+  aumentarIntervalo(): void {
+    this.intervalo += 1;
+  }
+  disminuirIntervalo(): void {
+    this.intervalo -= 1;
+  }
+  reestablecerIntervalo(): void {
+    this.intervalo = 1;
+  }
+  incrementarPor(): void {
+    this.contador += this.intervalo;
+  }
+  disminuirPor(): void {
+    this.contador -= this.intervalo;
+  }
+  reestablecerContador(): void {
+    this.contador = 0;
+  }
+}
