2565469957828241ae3f11a74c17fcbe0017493d
Karim <layne_2002@icloud.com>
2024-11-11 20:17:11 +0100

    ejemplo heroes

--- DiseñoDeInterfaces/02-Bases/src/app/app.component.ts
+++ DiseñoDeInterfaces/02-Bases/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContadorComponent } from './contador/contador.components';
+import { HeroeComponent } from "./heroes/heroe/heroe.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [ContadorComponent],
+  imports: [ContadorComponent, HeroeComponent],
 })
 export class AppComponent {
   public title: string = 'Mi primera app de Angular';
