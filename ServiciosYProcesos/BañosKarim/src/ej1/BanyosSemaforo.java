63385126c536cc188de169aa4f81cddab2bbf5ce
Karim <layne_2002@icloud.com>
2024-11-18 17:35:27 +0100

    banyos semaforo

--- ServiciosYProcesos/BañosKarim/src/ej1/BanyosSemaforo.java
+++ ServiciosYProcesos/BañosKarim/src/ej1/BanyosSemaforo.java
@@ -11,7 +11,20 @@ public class BanyosSemaforo {
 	public static void main(String[] args) {
 		while (true) {
 			String persona = generarNombreAleatorio();
-			new Thread(new UsoBanio(persona)).start();
+
+			new Thread(() -> {
+				try {
+					System.out.println(persona + " está esperando para entrar al baño.");
+					semaforo.acquire();
+					System.out.println(persona + " va al baño.");
+					Thread.sleep((long) (Math.random() * 5000 + 1000));
+					System.out.println(persona + " salió del baño.");
+				} catch (InterruptedException e) {
+					Thread.currentThread().interrupt();
+				} finally {
+					semaforo.release();
+				}
+			}).start();
 
 			try {
 				Thread.sleep(1000);
@@ -24,28 +37,5 @@ public class BanyosSemaforo {
 	private static String generarNombreAleatorio() {
 		String[] nombres = { "Pepito", "Jaimito", "Luisito", "Anita", "Maria" };
 		return nombres[(int) (Math.random() * nombres.length)];
-	}
-
-	static class UsoBanio implements Runnable {
-		private final String persona;
-
-		public UsoBanio(String persona) {
-			this.persona = persona;
-		}
-
-		@Override
-		public void run() {
-			try {
-				System.out.println(persona + " está esperando para entrar al baño.");
-				semaforo.acquire();
-				System.out.println(persona + " va al baño.");
-				Thread.sleep((long) (Math.random() * 5000 + 2000));
-				System.out.println(persona + " salió del baño.");
-			} catch (InterruptedException e) {
-				Thread.currentThread().interrupt();
-			} finally {
-				semaforo.release();
-			}
-		}
 	}
 }
