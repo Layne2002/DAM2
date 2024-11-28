afdeb53515383105cb61d2fb9be21181d99c5a03
Karim <layne_2002@icloud.com>
2024-11-28 18:44:38 +0100

    FormsModule

--- DiseñoDeInterfaces/02-Bases/src/app/dbz/components/AddPersonaje/dbz-addPersonaje.component.ts
+++ DiseñoDeInterfaces/02-Bases/src/app/dbz/components/AddPersonaje/dbz-addPersonaje.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Personaje } from '../../interfaces/personaje.interface';
 import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'dbz-add-personaje',
   templateUrl: 'dbz-addpersonaje.component.html',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
 })
 export class AddPersonajeComponent {
   public personaje: Personaje = {
