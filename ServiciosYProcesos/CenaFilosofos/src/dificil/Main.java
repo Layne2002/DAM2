39d42f0e57ede90b3cf451017f2171602a012da2
Karim <layne_2002@icloud.com>
2024-12-01 19:44:16 +0100

    filosofos chinos

--- ServiciosYProcesos/CenaFilosofos/src/dificil/Main.java
+++ ServiciosYProcesos/CenaFilosofos/src/dificil/Main.java
@@ -4,19 +4,21 @@ import java.util.concurrent.Semaphore;
 
 public class Main {
 	public static final Semaphore[] palillos = new Semaphore[5];
+	public static final int VECES_COMER = 5; // cuantas veces debe comer cada filosofo
 
 	public static void main(String[] args) {
+
 		for (int i = 0; i < 5; i++) {
 			palillos[i] = new Semaphore(1);
 		}
 		Filosofo[] filosofos = new Filosofo[5];
-		filosofos[0] = new Filosofo("Aristóteles", 0, 1, palillos);
-		filosofos[1] = new Filosofo("Nietzsche", 1, 2, palillos);
-		filosofos[2] = new Filosofo("Sócrates", 2, 3, palillos);
-		filosofos[3] = new Filosofo("Platón", 3, 4, palillos);
-		filosofos[4] = new Filosofo("Descartes", 4, 0, palillos);
+		filosofos[0] = new Filosofo("Huao Piao", 0, 1, palillos);
+		filosofos[1] = new Filosofo("Bin Xilling", 1, 2, palillos);
+		filosofos[2] = new Filosofo("XinXue", 2, 3, palillos);
+		filosofos[3] = new Filosofo("中国哲学家", 3, 4, palillos);
+		filosofos[4] = new Filosofo("BAZAR ALIMENTACIÓN JIN HUE PAIO BAO", 4, 0, palillos);
 		for (Filosofo f : filosofos) {
-			new Thread(() -> f.comer()).start();
+			new Thread(() -> f.comer(VECES_COMER)).start();
 		}
 	}
 
@@ -34,19 +36,24 @@ class Filosofo {
 		this.palillos = palillos;
 	}
 
-	public void comer() {
+	public void comer(int veces) {
+
 		int contador = 0;
 		try {
-			for (int i = 0; i < 5; i++) {
+			for (int i = 0; i < veces; i++) { // este bucle es para que cada filosofo coma 5 veces
 				System.out.println(nombre + " está filosofando...");
-				Thread.sleep(1000);
-
+				if (this.palilloDer == 0 && this.palilloIzd == 4) { // el quinto filosofo espera un poco mas para
+																	// empezar a comer, evitando el deadlock
+					Thread.sleep(3000);
+				} else {
+					Thread.sleep(1000);
+				}
 				palillos[palilloIzd].acquire();
 
-				System.out.println(nombre + " ha cogido el palillo " + (palilloIzd+1));
+				System.out.println(nombre + " ha cogido el palillo " + (palilloIzd + 1));
 				palillos[palilloDer].acquire();
-				System.out.println("> " + nombre + " ha cogido el palillo " + (palilloDer+1));
-				System.out.println(nombre + " está comiendo");
+				System.out.println(nombre + " ha cogido el palillo " + (palilloDer + 1));
+				System.out.println("> " + nombre + " está comiendo");
 				Thread.sleep(3000);
 
 				palillos[palilloIzd].release();
@@ -59,7 +66,7 @@ class Filosofo {
 		} catch (InterruptedException e) {
 			Thread.currentThread().interrupt();
 		}
-		System.out.println(nombre + " ha comido " + contador + " veces");
+		System.err.println("--> " + nombre + " ha comido " + contador + " veces");
 	};
 
 }
